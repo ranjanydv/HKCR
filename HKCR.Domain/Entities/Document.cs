f80cae56e1395bc482f3b6be50ba8818790541a4
Ranjan Yadav <theranzanydv@gmail.com>
2023-04-28 13:01:23 +0545

    doc.cs

---
+++ HKCR.Domain/Entities/Document.cs
@@ -0,0 +1,21 @@
+﻿using HKCR.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCR.Domain.Entities
+{
+    public class Document
+    {
+
+        public Guid DocId { get; set; } = new Guid();
+        public string Type { get; set; }
+        public string DocPath { get; set; }
+
+        // [ForeignKey("Department")]
+        // public int DepartmentId { get; set; }
+        // public virtual Department Department { get; set; }
+    }
+}
